7f532cd02208fbbf01071ac15dc4381e446dbb2e
Mitzi Estrada <m_estrada17@live.com>
2020-03-12 04:58:29 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  
+  attr_accessor :name
+  
+  @@all = [ ]
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs
+    Song.all { |song| song.genre == self}
+  end
+  
+  def artists
+    artists.all.collect { |song| song.artist = self}
+  end
+  
+  
+  
+end
