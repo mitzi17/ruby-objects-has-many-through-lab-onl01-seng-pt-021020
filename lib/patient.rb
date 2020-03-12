98d5af7dbaec3c3469bcf1ccea7b85e44ef3b3ba
Mitzi Estrada <m_estrada17@live.com>
2020-03-12 05:23:02 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.detect { |appt| appt.patient == self }
+    Appointment.all.select { |appt| appt.patient == self }
   end
   
   def doctors
